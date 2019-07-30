41b98a8588f929894872fe61735d86ac7480b071
Luis Magana <maganaluis92@gmail.com>
2019-07-29 20:05:09 -0400

    debug

--- scrapyd/webservice.py
+++ scrapyd/webservice.py
@@ -53,6 +53,7 @@ class Schedule(WsResource):
         args['settings'] = settings
         jobid = args.pop('jobid', uuid.uuid1().hex)
         args['_job'] = jobid
+        print(jobid)
         self.root.scheduler.schedule(project, spider, priority=priority, **args)
         return {"node_name": self.root.nodename, "status": "ok", "jobid": jobid}
 
